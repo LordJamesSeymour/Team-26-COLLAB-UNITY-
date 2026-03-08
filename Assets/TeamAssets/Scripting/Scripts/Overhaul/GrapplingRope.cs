6886e29cd38032428d64cb1617efdc7a0bec4611
JamesSeymour00 <james.s.bolivar@gmail.com>
2026-03-08 21:34:16 +0000

    Almost completed Swing gun
    Working gun, broken animation if you dont hit a target

--- Assets/TeamAssets/Scripting/Scripts/Overhaul/GrapplingRope.cs
+++ Assets/TeamAssets/Scripting/Scripts/Overhaul/GrapplingRope.cs
@@ -1,71 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GrapplingRope : MonoBehaviour
 {
-	[Header("References")]
 	private Spring spring;
 	private LineRenderer lr;
 	private Vector3 currentGrapplePosition;
-	[SerializeField] SwingGun SwingGun;
-	[SerializeField] int m_iRopeResolution;
-	[SerializeField] int m_fDamper;
-	[SerializeField] int m_fStrength;
-	[SerializeField] int m_fVelocity;
-	[SerializeField] int m_fWaveCount;
-	[SerializeField] int m_fWaveHeight;
-	[SerializeField] AnimationCurve a_cAffectCurve;
-	
+	public SwingGun SwingGun;
+	public int quality;
+	public float damper;
+	public float strength;
+	public float velocity;
+	public float waveCount;
+	public float waveHeight;
+	public AnimationCurve affectCurve;
+	private PlayerController PlayerController;
 
-
-
-	private void Awake()
+	void Awake()
 	{
 		lr = GetComponent<LineRenderer>();
 		spring = new Spring();
 		spring.SetTarget(0);
+		PlayerController = GetComponent<PlayerController>();
 	}
 
-	private void LateUpdate()
+	//Called after Update
+	void LateUpdate()
 	{
 		DrawRope();
 	}
 
-
-	private void DrawRope()
+	void DrawRope()
 	{
-		if (!SwingGun.joint)
+		//If not grappling, don't draw rope
+		if (!PlayerController.m_bActiveSwing)
 		{
 			currentGrapplePosition = SwingGun.gunTip.position;
 			spring.Reset();
-
-			if(lr.positionCount > 0)
+			if (lr.positionCount > 0)
 				lr.positionCount = 0;
 			return;
 		}
 
 		if (lr.positionCount == 0)
 		{
-			spring.SetVelocity(m_fVelocity);
-			lr.positionCount = m_iRopeResolution + 1;
+			spring.SetVelocity(velocity);
+			lr.positionCount = quality + 1;
 		}
 
-
-		spring.SetDamper(m_fDamper);
-		spring.SetStrength(m_fStrength);
+		spring.SetDamper(damper);
+		spring.SetStrength(strength);
 		spring.Update(Time.deltaTime);
 
-		var swingPoint = SwingGun.GetSwingPoint();
-		var gunTipPos = SwingGun.gunTip.position;
-		var up = Quaternion.LookRotation(swingPoint - gunTipPos).normalized * Vector3.up;
+		var grapplePoint = SwingGun.GetSwingPoint();
+		var gunTipPosition = SwingGun.gunTip.position;
+		var up = Quaternion.LookRotation((grapplePoint - gunTipPosition).normalized) * Vector3.up;
 
-		currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, SwingGun.GetSwingPoint(), Time.deltaTime * 8f);
+		currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 12f);
 
-		for (int i = 0; i < m_iRopeResolution; i++) 
+		for (var i = 0; i < quality + 1; i++)
 		{
-			var delta = i / (float)m_iRopeResolution;
-			var offset = up * m_fWaveHeight * Mathf.Sin(delta * m_fWaveCount * Mathf.PI) * spring.Value * a_cAffectCurve.Evaluate(delta);
+			var delta = i / (float)quality;
+			var offset = up * waveHeight * Mathf.Sin(delta * waveCount * Mathf.PI) * spring.Value *
+						 affectCurve.Evaluate(delta);
 
-			lr.SetPosition(i, Vector3.Lerp(gunTipPos, currentGrapplePosition, delta) + offset);
+			lr.SetPosition(i, Vector3.Lerp(gunTipPosition, currentGrapplePosition, delta) + offset);
 		}
 	}
 }
