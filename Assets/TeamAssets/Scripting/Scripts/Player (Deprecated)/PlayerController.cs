68ef47aa706b4ae38684d0afff455896204bd660
Steezyy17 <steveb1701@outlook.com>
2026-03-09 16:31:29 +0000

    Fixup stuff

--- Assets/TeamAssets/Scripting/Scripts/Player (Deprecated)/PlayerController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Group26.Player.Controller
-{
-    public class PlayerController : MonoBehaviour
-    {
-    //[SerializeField] Locomotion.PlayerLocomotion m_scrPlayerLocomotion;
-    }
-}
