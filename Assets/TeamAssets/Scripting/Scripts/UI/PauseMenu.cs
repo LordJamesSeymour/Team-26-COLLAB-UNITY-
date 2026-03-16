49289a8a3f17e9ea2b7d95b4f2db118cb4df5926
Joseph-S04 <115216137+Joseph-S04@users.noreply.github.com>
2026-03-16 17:06:49 +0000

    pause menu scene
    pause menu based on template, will be a prefab for game scenes to load and enable/disable at level run time on escape pressed.

--- Assets/TeamAssets/Scripting/Scripts/UI/PauseMenu.cs
+++ Assets/TeamAssets/Scripting/Scripts/UI/PauseMenu.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] Button m_resume;
+    [SerializeField] Button m_restart;
+    [SerializeField] Button m_controls;
+    [SerializeField] Button m_settings;
+    [SerializeField] Button m_return;
+
+    private void Awake()
     {
-        
+        m_resume.onClick.AddListener(Resume);
+        m_restart.onClick.AddListener(Restart);
+        m_controls.onClick.AddListener(Controls);
+        m_settings.onClick.AddListener(Settings);
+        m_return.onClick.AddListener(Return);
     }
 
-    // Update is called once per frame
-    void Update()
+    // listen on player for esc, if clicked call gameObject.SetActive(!gameObject.activeSelf); // switch menu state
+
+    public void Resume() // close pause menu (will need to stop and start gameplay)
     {
-        
+        Debug.Log("Resume");
+        gameObject.SetActive(false);
+    }
+    private void Restart() // reload this scene
+    {
+        Debug.Log("Restart");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    private void Controls() // open controls image
+    {
+        Debug.Log("Controls");
+    }
+    private void Settings()         // open settings sub menu
+    {
+        // only setting so far is sense on mouse and controller
+        Debug.Log("Settings");
+    }
+    private void Return() //load map scene
+    {
+        Debug.Log("Return");
     }
 }
