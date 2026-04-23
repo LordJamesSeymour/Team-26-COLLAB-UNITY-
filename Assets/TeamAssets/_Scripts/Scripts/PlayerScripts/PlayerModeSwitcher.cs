e0a0a55f460dd5fa7126be2750c0a7e8d002e779
StevenBrennan01 <steveb1701@outlook.com>
2026-04-23 16:01:25 +0100

    ball rolling stuff

--- Assets/TeamAssets/_Scripts/Scripts/PlayerScripts/PlayerModeSwitcher.cs
+++ Assets/TeamAssets/_Scripts/Scripts/PlayerScripts/PlayerModeSwitcher.cs
@@ -19,6 +19,12 @@ namespace Group26.Player.Utility
         [SerializeField] private GameObject capsuleModeObject;
         [SerializeField] private GameObject ballModeObject;
 
+        [SerializeField] private Transform capsuleModeTransform;
+        [SerializeField] private Transform sphereColliderTransform;
+        
+
+        private Rigidbody m_rigidbody;
+
         public PlayerMode currentMode = PlayerMode.CapsuleMode;
 
         private GrappleGun m_grappleGunScript;
@@ -49,6 +55,7 @@ namespace Group26.Player.Utility
                 Debug.LogError("Grapple gun script is not attached");
             }
 
+            m_rigidbody = GetComponent<Rigidbody>();
         }
 
         private void OnEnable()
@@ -81,11 +88,25 @@ namespace Group26.Player.Utility
                     capsuleModeObject.SetActive(true);
                     playerController.enabled = true;
                     ballRollController.enabled = false;
+
+                    m_rigidbody.angularVelocity = Vector3.zero;
+                    sphereColliderTransform.localRotation = Quaternion.identity;
+
                     break;
 
                 case PlayerMode.BallMode:
+
+                    Vector3 forwardDirection = capsuleModeTransform.forward;
+                    forwardDirection.y = 0f;
+
+                    if(forwardDirection.sqrMagnitude > 0.01f)
+                    {
+                        ballModeObject.transform.rotation = Quaternion.LookRotation(forwardDirection.normalized, Vector3.up);
+                    }
+
                     capsuleModeObject.SetActive(false);
                     ballModeObject.SetActive(true);
+
                     playerController.m_bIsGrounded = false;
                     playerController.enabled = false;
                     ballRollController.enabled = true;
