4f2c60735e58a55c2822c93053ef66a59e101d16
Joseph-S04 <115216137+Joseph-S04@users.noreply.github.com>
2026-04-27 19:25:35 +0100

    impact

--- Assets/TeamAssets/_Scripts/Scripts/SFX/ImpactSound.cs
+++ Assets/TeamAssets/_Scripts/Scripts/SFX/ImpactSound.cs
@@ -7,6 +7,8 @@ public class ImpactSound : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         if (!m_ball.activeSelf) return;
+        Debug.Log(Vector3.Angle(other.relativeVelocity, -other.contacts[0].normal));
+        if (Vector3.Angle(other.relativeVelocity, -other.contacts[0].normal) < 140) return;
 
         AudioManager.instance.PlaySoundAtPoint(AudioManager.SoundType.LAND, other.contacts[0].point, 1, .1f, 1, .1f, .8f);
     }
