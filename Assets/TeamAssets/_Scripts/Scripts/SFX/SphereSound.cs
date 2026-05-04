804dd43aba372adc180e53a36658c94b70a0f647
Joseph-S04 <115216137+Joseph-S04@users.noreply.github.com>
2026-05-04 18:49:38 +0100

    tuned some sounds

--- Assets/TeamAssets/_Scripts/Scripts/SFX/SphereSound.cs
+++ Assets/TeamAssets/_Scripts/Scripts/SFX/SphereSound.cs
@@ -25,8 +25,8 @@ public class SphereSound : MonoBehaviour
         if (!ballRollController.IsGrounded()) { source.volume = 0; return; }
 
         float vel = rb.linearVelocity.magnitude;
-        source.volume = Mathf.Clamp(vel / 40, 0, 5);
-        source.pitch = Mathf.Clamp(vel / 30, .4f, 5);
+        source.volume = Mathf.Clamp(vel / 30, 0, 1.5f);
+        source.pitch = Mathf.Clamp(vel / 30, .4f, .7f);
     }
     private void OnDisable()
     {
