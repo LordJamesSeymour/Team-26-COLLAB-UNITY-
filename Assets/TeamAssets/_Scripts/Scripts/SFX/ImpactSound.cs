2c20c905a397e9e769004b81d2dc8037530c8c09
Joseph-S04 <115216137+Joseph-S04@users.noreply.github.com>
2026-05-04 15:48:43 +0100

    all sound fixes

---
+++ Assets/TeamAssets/_Scripts/Scripts/SFX/ImpactSound.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour
+{
+    [SerializeField] private GameObject m_ball;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!m_ball.activeSelf) return;
+        //Debug.Log(Vector3.Angle(other.relativeVelocity, -other.contacts[0].normal));
+        if (Vector3.Angle(other.relativeVelocity, -other.contacts[0].normal) < 130) return;
+
+        AudioManager.instance.PlaySoundAtPoint(AudioManager.SoundType.LAND, other.contacts[0].point, GetComponent<Rigidbody>().linearVelocity.magnitude / 20, .1f, 1, .1f, .8f);
+    }
+}
