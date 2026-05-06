64727ddff694b10c8070c35a7e8eadffc1fa1c79
Joseph-S04 <115216137+Joseph-S04@users.noreply.github.com>
2026-05-06 18:51:21 +0100

    impact change
    changed the impact sound because it was having issues with popping at the end of the og sound

--- Assets/TeamAssets/_Scripts/Scripts/SFX/ImpactSound.cs
+++ Assets/TeamAssets/_Scripts/Scripts/SFX/ImpactSound.cs
@@ -10,6 +10,7 @@ public class ImpactSound : MonoBehaviour
         //Debug.Log(Vector3.Angle(other.relativeVelocity, -other.contacts[0].normal));
         if (Vector3.Angle(other.relativeVelocity, -other.contacts[0].normal) < 130) return;
 
-        AudioManager.instance.PlaySoundAtPoint(AudioManager.SoundType.CRASH, other.contacts[0].point, Mathf.Clamp((other.relativeVelocity.magnitude - 15) / 90, 0, .35f), .1f, .6f, .07f, 0);
+        //AudioManager.instance.PlaySoundAtPoint(AudioManager.SoundType.CRASH, other.contacts[0].point, Mathf.Clamp((other.relativeVelocity.magnitude - 15) / 90, .02f, .35f), .02f, .6f, .07f, 0);
+        AudioManager.instance.PlaySoundAtPoint(AudioManager.SoundType.CRASH, other.contacts[0].point, Mathf.Clamp((other.relativeVelocity.magnitude - 10) / 30, .03f, 1), .01f, 1.8f, .07f, 0);
     }
 }
